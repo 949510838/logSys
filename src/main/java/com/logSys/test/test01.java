7e38ab1b117b38f5466fc109c82ecfb833964fe5
tom_glb <tom_glb@qq.com>
2019-11-09 17:26:55 +0800

    修改测试类包位置

--- src/main/java/com/logSys/test/test01.java
+++ src/main/java/com/logSys/test/test01.java
@@ -1,31 +1,47 @@
 package com.logSys.test;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.logSys.dao.LogDao;
+import com.logSys.service.LogService;
 
+@Controller
 public class test01 {
-	LogDao logDao;
-
-
-	public void setLogDao(LogDao logDao) {
-		this.logDao = logDao;
+	
+	@Autowired
+	@Qualifier("logService")
+	private LogService logService;
+	
+	
+	//springmvc默认返回字符串编码格式是 text/plain;charset=ISO-8859-1（在浏览器响应头Content-Type），返回中文会乱码，使用produces属性可以设置返回数据的编码为utf-8
+	//也可以配置统一的StringHttpMessageConverter消息转换（在配置文件中配置）
+	@RequestMapping(value="/test01",produces="text/json;charset=UTF-8")
+	@ResponseBody
+	public String test01(HttpServletRequest requset,HttpServletResponse response) {
+		System.out.println(logService);
+		System.out.println(requset);
+		return "张三";
 	}
-
-
-	public LogDao getLogDao() {
-		return logDao;
+	
+	@RequestMapping(value="/test02")
+	public String test02(HttpServletRequest req) {
+		System.out.println(1232);
+		
+		//return "forward:hello.html";
+		return "redirect:hello.html";
 	}
-
-
-	public static void main(String[] args) {
-		System.out.println(123);
-
-		ApplicationContext ac = new FileSystemXmlApplicationContext("springmvc.xml");
-		test01 ts = (test01)ac.getBean("test01");
-		ts.getLogDao().deleteTask();
-
-	}
+	
+	
+	
 
 }
